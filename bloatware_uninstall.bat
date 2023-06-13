656ba42503df3006992bd5487e7c7db3e26ddd99
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-06-13 13:58:31 -0400

    Fix bloatware_uninstall.bat Packages to FullPackageNames

--- bloatware_uninstall.bat
+++ bloatware_uninstall.bat
@@ -1,16 +1,18 @@
 @ECHO off
 
 echo 1. Uninstall bloatware
-powershell Remove-AppxPackage Microsoft.Microsoft3DViewer
-powershell Remove-AppxPackage Microsoft.BingWeather
-powershell Remove-AppxPackage Microsoft.GetHelp
-powershell Remove-AppxPackage Microsoft.WindowsMaps
-powershell Remove-AppxPackage Microsoft.YourPhone
-powershell Remove-AppxPackage Microsoft.XboxGamingOverlay
-powershell Remove-AppxPackage Microsoft.MixedReality.Portal
-powershell Remove-AppxPackage Microsoft.SkypeApp
-powershell Remove-AppxPackage Microsoft.WindowsFeedbackHub
-powershell Remove-AppxPackage Microsoft.MicrosoftSolitaireCollection
-powershell Remove-AppxPackage SpotifyAB.SpotifyMusic
-powershell Remove-AppxPackage Microsoft.Getstarted
-powershell Remove-AppxPackage Microsoft.MicrosoftStickyNotes
+powershell Remove-AppxPackage Microsoft.Microsoft3DViewer_7.2211.24012.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.BingWeather_4.53.51461.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.GetHelp_10.2303.10961.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.WindowsMaps_11.2303.5.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.YourPhone_1.23042.108.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.Microsoft.XboxGameOverlay_1.54.4001.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.XboxApp_48.89.25001.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.MixedReality.Portal_2000.21051.1282.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.SkypeApp_15.98.3402.0_x64__kzf8qxf38zg5c
+powershell Remove-AppxPackage Microsoft.WindowsFeedbackHub_1.2304.1243.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.MicrosoftSolitaireCollection_4.16.3140.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Disney.37853FC22B2CE_1.54.2.0_x64__6rarf9sa4v8jt
+powershell Remove-AppxPackage SpotifyAB.SpotifyMusic_1.213.661.0_x86__zpdnekdrzrea0
+powershell Remove-AppxPackage Microsoft.Getstarted_8.2.22942.0_x64__8wekyb3d8bbwe
+powershell Remove-AppxPackage Microsoft.MicrosoftStickyNotes_3.6.73.0_x64__8wekyb3d8bbwe
