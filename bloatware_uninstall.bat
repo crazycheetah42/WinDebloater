c11448105e6c502f948cb46d995dd4ace639e56e
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-06-13 13:45:02 -0400

    Add more APPX Packages to bloatware_uninstall.bat

--- bloatware_uninstall.bat
+++ bloatware_uninstall.bat
@@ -1,9 +1,16 @@
 @ECHO off
 
 echo 1. Uninstall bloatware
-powershell Remove-AppxPackage Microsoft.Microsoft3DBuilder
+powershell Remove-AppxPackage Microsoft.Microsoft3DViewer
 powershell Remove-AppxPackage Microsoft.BingWeather
 powershell Remove-AppxPackage Microsoft.GetHelp
 powershell Remove-AppxPackage Microsoft.WindowsMaps
 powershell Remove-AppxPackage Microsoft.YourPhone
 powershell Remove-AppxPackage Microsoft.XboxGamingOverlay
+powershell Remove-AppxPackage Microsoft.MixedReality.Portal
+powershell Remove-AppxPackage Microsoft.SkypeApp
+powershell Remove-AppxPackage Microsoft.WindowsFeedbackHub
+powershell Remove-AppxPackage Microsoft.MicrosoftSolitaireCollection
+powershell Remove-AppxPackage SpotifyAB.SpotifyMusic
+powershell Remove-AppxPackage Microsoft.Getstarted
+powershell Remove-AppxPackage Microsoft.MicrosoftStickyNotes
