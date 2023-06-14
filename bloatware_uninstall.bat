4c9437aed16f119e463e70e30c2e9f5ac7960e3b
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-06-14 14:22:08 -0400

    Fix bloatware

--- bloatware_uninstall.bat
+++ bloatware_uninstall.bat
@@ -1,24 +1,27 @@
 @ECHO off
 
 echo 1. Uninstall bloatware
-powershell Remove-AppxPackage Microsoft.Microsoft3DViewer_7.2211.24012.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.BingWeather_4.53.51461.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.GetHelp_10.2303.10961.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.WindowsMaps_11.2303.5.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.YourPhone_1.23042.108.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.XboxApp_48.89.25001.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.MixedReality.Portal_2000.21051.1282.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.SkypeApp_15.98.3402.0_x64__kzf8qxf38zg5c
-powershell Remove-AppxPackage Microsoft.WindowsFeedbackHub_1.2304.1243.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.MicrosoftSolitaireCollection_4.16.3140.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Disney.37853FC22B2CE_1.54.2.0_x64__6rarf9sa4v8jt
-powershell Remove-AppxPackage SpotifyAB.SpotifyMusic_1.213.661.0_x86__zpdnekdrzrea0
-powershell Remove-AppxPackage Microsoft.Getstarted_8.2.22942.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.MicrosoftStickyNotes_3.6.73.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.Office.OneNote_16001.14326.21386.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.MicrosoftOfficeHub_18.2305.1222.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.MSPaint_6.2203.1037.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.WindowsSoundRecorder_10.2103.28.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.People_10.2202.31.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.WindowsCalculator_11.2210.0.0_x64__8wekyb3d8bbwe
-powershell Remove-AppxPackage Microsoft.Windows.PeopleExperienceHost_10.0.19041.1023_neutral_neutral_cw5n1h2txyewy
+powershell Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+powershell Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.MSPaint | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.People | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.WindowsCalculato | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.Windows.PeopleExperienceHost | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.Todos | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.PowerAutomateDesktop | Remove-AppxPackage
+powershell Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+powershell Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage
