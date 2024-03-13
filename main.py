0d2fcb03a421aba65fd5e620747121588f9e1d6d
Aryaman Sriram <crazycheetah42@outlook.com>
2024-03-13 16:28:52 -0400

    Add in power plan

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import tix
 import sv_ttk, darkdetect
-import ctypes
+import os
 
 root = tix.Tk()
 root.geometry("800x600")
 root.title("WinDebloater")
 root.wm_title("WinDebloater")
-root.attributes('-topmost',True)
 
 header_lbl = ttk.Label(root, text="WinDebloater", font=("Segoe UI", 22))
 header_lbl.pack()
@@ -17,15 +16,6 @@ header_lbl.pack()
 info_lbl = ttk.Label(root, text="Welcome to WinDebloater! Please hover over each option to see what it does.", font=("Segoe UI", 14))
 info_lbl.pack()
 
-def is_running_as_admin() -> bool:
-    # If the below function returns 0, it means that the script isn't running as admin.
-    # It'll return 1 otherwise.
-    # NOTE: This is calling a win32 API, meaning this won't work on UNIX-based systems (MacOS & Linux), as well as FreeBSD.
-    # Credit to og-mrk for this amazing function
-    return ctypes.windll.shell32.IsUserAnAdmin() != 0
-if not is_running_as_admin:
-    from tkinter import messagebox
-    messagebox.showerror("Need Admin permissions", "Please close and relaunch this program as Administrator.")
 def start_debloat(commands):
     for command in commands:
         subprocess.run(["powershell", "-Command", command], shell=True)
@@ -98,6 +88,32 @@ def advanced_debloat():
 def winutil_run():
     subprocess.run(["powershell", "-Command", "irm christitus.com/win | iex"], shell=True)
 
+def set_pwrplan():
+    def ultperfmode():
+        os.system('cmd /c "powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"')
+        os.system('cmd /c "powercfg -s e9a42b02-d5df-448d-aa00-03f14749eb61"')
+    def highperfmode():
+        os.system('cmd /c "powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"')
+    def balancedmode():
+        os.system('cmd /c "powercfg -s 381b4222-f694-41f0-9685-ff5bb260df2e"')
+    def pwrsvrmode():
+        os.system('cmd /c "powercfg -s a1841308-3541-4fab-bc81-f71556f20b4a"')
+    pwrplanwindow = tk.Tk()
+    pwrplanwindow.geometry("400x300")
+    pwrplanwindow.title("Choose a Power Plan")
+    hdr_lbl = ttk.Label(pwrplanwindow, text="Choose a Power Plan", font=("Segoe UI", 20))
+    hdr_lbl.pack()
+    ultperf = ttk.Button(pwrplanwindow, text="Ultimate Performance", command=ultperfmode)
+    highperf = ttk.Button(pwrplanwindow, text="High Performance", command=highperfmode)
+    balanced = ttk.Button(pwrplanwindow, text="Balanced", command=balancedmode)
+    pwrsvr = ttk.Button(pwrplanwindow, text="Power Saver", command=pwrsvrmode)
+    ultperf.pack()
+    highperf.pack()
+    balanced.pack()
+    pwrsvr.pack()
+
+    pwrplanwindow.mainloop()
+
 basic_debloat_btn = ttk.Button(root, text="Basic Debloat", command=basic_debloat)
 basic_tooltip = tix.Balloon(root)
 basic_tooltip.subwidget('label')['bg'] = 'black'
@@ -125,6 +141,13 @@ winutil_tooltip.subwidget('label')['bg'] = 'black'
 winutil_tooltip.subwidget('message')['bg'] = 'black'
 winutil_tooltip.bind_widget(winutil_btn, balloonmsg="This runs ChrisTitusTech's Windows Utility which is an all-in-one tool to install, tweak, configure, and change your update cycle. This is recommended after using one of the debloat modes.")
 winutil_btn.pack()
+
+pwrplan_btn = ttk.Button(root, text="Set Power Plan", command=set_pwrplan)
+pwrplan_tooltip = tix.Balloon(root)
+pwrplan_tooltip.subwidget('label')['bg'] = 'black'
+pwrplan_tooltip.subwidget('message')['bg'] = 'black'
+pwrplan_tooltip.bind_widget(pwrplan_btn, balloonmsg="Set a power plan to adjust for high/ultimate performance or balanced/power saver.")
+pwrplan_btn.pack()
 
 if darkdetect.isDark():
     sv_ttk.set_theme("dark")
